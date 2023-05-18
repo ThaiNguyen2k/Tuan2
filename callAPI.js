7c0b8ce291bb070d4a670629993973dd4cc90546
thainguyen2k <nguyendragon2000@gmail.com>
2023-05-18 11:36:55 +0700

    upload

--- callAPI.js
+++ callAPI.js
@@ -1,6 +1,8 @@
 // API https://fakestoreapi.com/products 
 
-
+window.onload= function(){
+  fetchData()
+}
 
 async function getAll() {
   return fetch('https://fakestoreapi.com/products')
@@ -9,32 +11,24 @@ async function getAll() {
 
 async function fetchData() {
   const products = await getAll();
-  console.log(products);
 
   const productsDiv = document.getElementById("products");
   for (let item of products) {
     productsDiv.innerHTML += `
-            <div class="im">
-            <p>${item.id}</p>
-            <img class="img" src="${item.image}"/>
-            <div class="tab">
-            <h2>${item.title}</h2>
-            <p>${item.description}</p>
-            <p>${item.price}</p>
-            <p>${item.category}</p>
-            </div>
+            <div class="im" onclick="goToDetail(${item.id})">
+                <p>${item.id}</p>
+                <img class="img" src="${item.image}"/>
+                <div class="tab">
+                    <h2>${item.title}</h2>
+                    <p>${item.description}</p>
+                    <p>${item.price}</p>
+                    <p>${item.category}</p>
+                </div>
             </div>
             `;
   }
 }
-fetchData()
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get('id');
-fetch(`https://fakestoreapi.com/products/${id}`)
-              .then(res => res.json())
-    .then(json => console.log(json))
   function goToDetail(id) {
-    window.location.assign(`details.html ? id = ${id}`);
+    window.location.assign(`details.html?id=${id}`);
   }
