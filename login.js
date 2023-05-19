f12b00606874cdd3c692ec378e1254d429dbf6fa
thainguyen2k <nguyendragon2000@gmail.com>
2023-05-19 09:20:53 +0700

    upload

--- login.js
+++ login.js
@@ -1,18 +1,37 @@
-async function getAll() {
-    fetch('https://dummyjson.com/auth/login', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    
-    username: 'kminchelle',
-    password: '0lelplR',
-    // expiresInMins: 60, // optional
-  })
-})
-.then(res => res.json())
-.then(console.log);
-  }
+const userName = document.getElementById("userName"); //username
+const pass = document.getElementById("pass"); //password
+const sm = document.getElementById("sm"); //submit
 
-  function getLogin(){
+function getLogin() {
+  if (userName.value === "" || pass.value === "") {
+    alert("Please fill in all fields");
+  }
+  else if (userName.value === "kminchelle" && pass.value === "0lelplR") {
     window.location.assign(`web.html`);
+  } else  {
+
+    alert("Incorrect username or password");
+  }
+}
+function getAll() {
+  return fetch('https://dummyjson.com/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      
+      username: 'kminchelle',
+      password: '0lelplR',
+      // expiresInMins: 60, // optional
+    })
+  })
+  .then(res => res.json())
+  .then(console.log);
+}
+window.onload= function(){
+  getAll();
+}
+
+async function fetchData() {
+  const products = await getAll();
+    
   }
