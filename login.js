cc0cb41d21f1ca13745acd4610aaa333d1d8177f
thainguyen2k <nguyendragon2000@gmail.com>
2023-05-19 14:54:58 +0700

    upload

--- login.js
+++ login.js
@@ -1,67 +1,66 @@
 
 let contents = document.getElementsByClassName("content");
-for(let i = 0; i < contents.length; i++) {
-    contents[i].addEventListener("click", function(e){
-        clickEvent(e);
-    });
+for (let i = 0; i < contents.length; i++) {
+  contents[i].addEventListener("click", function (e) {
+    clickEvent(e);
+  });
 }
 function cancelBtn() {
-    document.getElementById("form").reset();
+  document.getElementById("form").reset();
 }
 
 function closeBtn() {
-    document.getElementById("top").style.display = "none";
+  document.getElementById("top").style.display = "none";
 }
 function openSett() {
-    document.getElementById("top").style.display = "initial";
+  document.getElementById("top").style.display = "initial";
 
 }
 
 function clickEvent(e) {
-    for(let i = 0; i < contents.length; i++){
-        contents[i].style.color = "black";
-    }
-    e.currentTarget.style.color = "green";
+  for (let i = 0; i < contents.length; i++) {
+    contents[i].style.color = "black";
+  }
+  e.currentTarget.style.color = "green";
 }
-function reset(){
-    document.getElementById("test").style.fontWeight="normal";
-    
+function reset() {
+  document.getElementById("test").style.fontWeight = "normal";
+
 }
 
 const userName = document.getElementById("userName"); //username
 const pass = document.getElementById("pass"); //password
 const sm = document.getElementById("sm"); //submit
 
-function getLogin() {
+async function getLogin() {
   if (userName.value === "" || pass.value === "") {
     alert("Please fill in all fields");
   }
   else if (userName.value === "kminchelle" && pass.value === "0lelplR") {
+    const data = await getAll()
+    localStorage.setItem('Token', JSON.stringify(data.token))
     window.location.assign(`web.html`);
-  } else  {
-
+  } else {
     alert("Incorrect username or password");
   }
 }
-function getAll() {
+async function getAll() {
   return fetch('https://dummyjson.com/auth/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      
+
       username: 'kminchelle',
       password: '0lelplR',
       // expiresInMins: 60, // optional
     })
   })
-  .then(res => res.json())
+    .then(res => res.json())
 }
-window.onload= function(){
-  getAll();
-  fetchData();
+window.onload = function () {
 }
 
-async function fetchData() {
-  const data = await getAll();
-  console.log(data);
-  }
+async function logOut() {
+  localStorage.removeItem('Token')
+  window.location.assign(`login.html`);
+}
