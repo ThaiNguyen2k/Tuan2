3fdcf570e33f6cc182c01f9fceff2e6cb357aaca
thainguyen2k <nguyendragon2000@gmail.com>
2023-05-19 10:46:07 +0700

    upload

--- login.js
+++ login.js
@@ -1,3 +1,33 @@
+
+let contents = document.getElementsByClassName("content");
+for(let i = 0; i < contents.length; i++) {
+    contents[i].addEventListener("click", function(e){
+        clickEvent(e);
+    });
+}
+function cancelBtn() {
+    document.getElementById("form").reset();
+}
+
+function closeBtn() {
+    document.getElementById("top").style.display = "none";
+}
+function openSett() {
+    document.getElementById("top").style.display = "initial";
+
+}
+
+function clickEvent(e) {
+    for(let i = 0; i < contents.length; i++){
+        contents[i].style.color = "black";
+    }
+    e.currentTarget.style.color = "green";
+}
+function reset(){
+    document.getElementById("test").style.fontWeight="normal";
+    
+}
+
 const userName = document.getElementById("userName"); //username
 const pass = document.getElementById("pass"); //password
 const sm = document.getElementById("sm"); //submit
@@ -25,13 +55,13 @@ function getAll() {
     })
   })
   .then(res => res.json())
-  .then(console.log);
 }
 window.onload= function(){
   getAll();
+  fetchData();
 }
 
 async function fetchData() {
-  const products = await getAll();
-    
+  const data = await getAll();
+  console.log(data);
   }
